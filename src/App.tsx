9095072a02fb1c99ac808e655e7a00b4cffea219
Andy <aenndie@gmail.com>
2024-09-23 15:36:35 +0200

    installed chakra ui

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,9 @@
-import { useState } from "react";
-import Button from "./components/Button";
-import Connect from "./components/Connect";
-import Alert from "./components/Alert";
-/*import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
-import "./App.css";*/
+import { Button } from "@chakra-ui/react";
 
 function App() {
-  const [alertVisible, setAlertVisibility] = useState(false);
-
   return (
     <div>
-      <Connect />
-      {alertVisible && (
-        <Alert onClose={() => setAlertVisibility(false)}>
-          Hello <b>World</b>
-        </Alert>
-      )}
-      <Button color="success" onClick={() => setAlertVisibility(true)}>
-        Show Alert!
-      </Button>
+      <Button colorScheme="blue">Button</Button>
     </div>
   );
 }
--- src/main.tsx
+++ src/main.tsx
@@ -1,5 +1,6 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
+import { ChakraProvider } from "@chakra-ui/react";
 import App from "./App.js";
 import "bootstrap/dist/css/bootstrap.css";
 
@@ -7,8 +8,12 @@ const connectButton = document.createElement("radix-connect-button");
 const header = document.querySelector("header")!;
 header.appendChild(connectButton);
 
+//
+
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <App />
+    <ChakraProvider>
+      <App />
+    </ChakraProvider>
   </StrictMode>
 );
